e2d496d1d6748f108a49ea1e74512bcb3c99d501
chikalusa <bsc-com-03-19@cc.ac.mw>
2022-12-26 17:40:16 +0200

    Frontend code and Backend code

---
+++ backendPhishingDemo/src/main/java/com/example/backendPhishingDemo/BackendPhishingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendPhishingDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPhishingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPhishingDemoApplication.class, args);
+	}
+
+}
