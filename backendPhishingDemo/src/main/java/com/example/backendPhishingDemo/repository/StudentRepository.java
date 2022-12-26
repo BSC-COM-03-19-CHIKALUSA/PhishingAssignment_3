03dbef4a5a5f35748b2ed948ec7219e9edf07d7a
chikalusa <bsc-com-03-19@cc.ac.mw>
2022-12-26 17:33:11 +0200

    Frontend code

--- backendPhishingDemo/src/main/java/com/example/backendPhishingDemo/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backendPhishingDemo.repository;
-
-import com.example.backendPhishingDemo.student.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student ,String> {
-}
